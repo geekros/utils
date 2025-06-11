1bee116cc8086a04789afec7b9531c07a7099fe2
MakerYang <open@wileho.com>
2025-06-11 10:45:58 +0800

    dev

---
+++ pkg/encryption/encryption.go
@@ -0,0 +1,27 @@
+// Copyright 2025 GEEKROS, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package encryption
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+func EncryptMD5(input string) string {
+	hash := md5.New()
+	hash.Write([]byte(input))
+	hashBytes := hash.Sum(nil)
+	return hex.EncodeToString(hashBytes)
+}
